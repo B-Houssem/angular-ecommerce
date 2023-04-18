97aa19aa14c2a8e8df50162739631620279c02d0
B-Houssem <houssembendaas00@gmail.com>
2023-04-18 17:28:54 +0100

    initial commit

---
+++ src/app/components/login-status/login-status.component.ts
@@ -0,0 +1,54 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { OKTA_AUTH, OktaAuthStateService } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
+
+@Component({
+  selector: 'app-login-status',
+  templateUrl: './login-status.component.html',
+  styleUrls: ['./login-status.component.css']
+})
+export class LoginStatusComponent implements OnInit{
+  
+  isAuthenticated: boolean = false;
+  userFullName: string = '';
+
+  storage: Storage = sessionStorage;
+
+  constructor(private oktaAuthService: OktaAuthStateService, @Inject(OKTA_AUTH) private oktaAuth: OktaAuth){}
+
+  ngOnInit(): void {
+
+    //subscribe to the authentication state changes
+    this.oktaAuthService.authState$.subscribe(
+      (result) => {
+        this.isAuthenticated = result.isAuthenticated!;
+        this.getUserDetails();
+      }
+    )
+  }
+  getUserDetails() {
+    if(this.isAuthenticated){
+      // fetch the logged in user details (user's claims)
+      //
+      //user fullname is exposed as a property name
+      this.oktaAuth.getUser().then(
+        (res) =>
+        {
+          this.userFullName = res.name as string;
+
+          //retrieve the user's email from authentication response
+          const theEmail = res.email;
+
+          //now store the email in the browser storage
+          this,this.storage.setItem('userEmail', JSON.stringify(theEmail));
+        }
+      );
+    }
+  }
+
+  logout(){
+    //terminate the session with Okta and remove current tokens
+    this.oktaAuth.signOut();
+  }
+
+}
---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,11 @@
+export default {
+
+    oidc: {
+        clientId: '0oa952q6kacYuoRiv5d7',
+        issuer: 'https://dev-43116709.okta.com/oauth2/default',
+        redirectUri: 'https://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+
+}
